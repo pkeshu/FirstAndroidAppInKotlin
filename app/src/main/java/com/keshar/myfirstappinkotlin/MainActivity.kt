bc322fbac330fd405d108774cee72ce3eb12c0ba
keshar <paudelkeshar2055@gmail.com>
2019-06-20 17:31:59 +0545

    First app is completed.

--- app/src/main/java/com/keshar/myfirstappinkotlin/MainActivity.kt
+++ app/src/main/java/com/keshar/myfirstappinkotlin/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.keshar.myfirstappinkotlin
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val MESSAGEID: String = "com.keshar.myfirstappinkotlin.messageId"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         submitBtn.setOnClickListener {
             val message: String = yourMessageEdt.text.toString().trim()
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra(MESSAGEID, message)
+            startActivity(intent)
+            finish()
         }
     }
 
--- app/src/main/java/com/keshar/myfirstappinkotlin/SecondActivity.kt
+++ app/src/main/java/com/keshar/myfirstappinkotlin/SecondActivity.kt
@@ -1,12 +1,23 @@
 package com.keshar.myfirstappinkotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
+    private var message: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        getIntentValue()
+        showMessageTxt.text = message
+
+    }
+
+    private fun getIntentValue() {
+        var data: Bundle? = intent.extras
+        message = data?.getString(MainActivity.MESSAGEID) ?: ""
     }
 }
+
